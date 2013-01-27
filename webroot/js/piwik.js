22cf65c51bd6466e853f4d81b87133098e30bc86
dogmatic69 <dogmatic69@gmail.com>
2013-01-27 21:54:57 +0000

    adding site search

--- webroot/js/piwik.js
+++ webroot/js/piwik.js
@@ -1,15 +1,24 @@
 try {
 	var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", trackingId);
 
-	if(typeof customVariables !== 'undefined') {
-		$.each(customVariables.page, function(k, v) {
-			piwikTracker.setCustomVariable(parseInt(v.id), v.name, v.value, v.scope);
-		});
-		$.each(customVariables.visit, function(k, v) {
-			piwikTracker.setCustomVariable(parseInt(v.id), v.name, v.value, v.scope);
-		});
+	if (typeof piwikSearch !== 'undefined') {
+		piwikTracker.trackSiteSearch( 
+			piwikSearch.keyword,
+			piwikSearch.category,
+			piwikSearch.results
+		);
+		piwikTracker.trackPageView();
+		piwikTracker.enableLinkTracking();
+	} else {
+		if(typeof customVariables !== 'undefined') {
+			$.each(customVariables.page, function(k, v) {
+				piwikTracker.setCustomVariable(parseInt(v.id), v.name, v.value, v.scope);
+			});
+			$.each(customVariables.visit, function(k, v) {
+				piwikTracker.setCustomVariable(parseInt(v.id), v.name, v.value, v.scope);
+			});
+		}
+		piwikTracker.trackPageView();
+		piwikTracker.enableLinkTracking();
 	}
-
-	piwikTracker.trackPageView();
-	piwikTracker.enableLinkTracking();
 } catch(err) {}
